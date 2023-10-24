c0426fbbca6909e35a9f9d79af818f2968caf7c2
JuanTB300405 <127706094+JuanTB300405@users.noreply.github.com>
2023-10-24 18:58:56 -0500

    correciones
    correciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import style from "./styles/style.css"
-import Navbar from "./components/Navbar"
-import Main from "./pages/Main"
-import Footer from "./components/footer"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import style from "./styles/style.css";
+import Navbar from "./components/Navbar";
+import Main from "./pages/Main";
+import Footer from "./components/Footer";
+import PublicLayout from "./layouts/PublicLayout";
+import Destacados from "./pages/Destacados"
+import Catalogo from "./pages/Catalogo"
+import Login from "./pages/Login"
+import AboutUs from "./pages/AboutUs"
 
 function App() {
   return (
-      <>
-        <Navbar/>
-        <Main/>
-        <Footer/>
-      </>
-  )
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PublicLayout />}>
+            <Route path="" element={<Main />} />
+            <Route path="destacados" element={<Destacados />}/>
+            <Route path="catalogo" element={<Catalogo />}/>
+            <Route path="login" element={<Login />}/>
+            <Route path="aboutus" element={<AboutUs />}/>
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
