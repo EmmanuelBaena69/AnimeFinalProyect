1eddf33a9a155114223c35741a1b2a21791f01c1
EmmanuelBaena69 <travispastrana075@gmail.com>
2023-10-24 19:50:00 -0500

    feat: se agregó react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import style from "./styles/style.css"
 import Navbar from "./components/Navbar"
 import Main from "./pages/Main"
 import Footer from "./components/footer"
+import Slider from "./components/slider"
 
 function App() {
   return (
       <>
         <Navbar/>
+        <Slider/>
         <Main/>
         <Footer/>
       </>
