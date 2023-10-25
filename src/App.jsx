9479c9b4b3e815e002663ab5aa4ba46e2c4a23e3
EmmanuelBaena69 <travispastrana075@gmail.com>
2023-10-24 20:07:29 -0500

    Import para que se abrieran las demas pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import style from "./styles/style.css";
 import PublicLayout from "./layouts/PublicLayout";
 import Main from "./pages/Main";
+import Destacados from "./pages/Destacados"
+import Catalogo from "./pages/Catalogo"
+import Login from "./pages/Login"
+import AboutUs from "./pages/AboutUs"
+
 
 function App() {
   return (
@@ -10,7 +15,10 @@ function App() {
         <Routes>
           <Route path="/" element={<PublicLayout />}>
             <Route path="" element={<Main />} />
-
+            <Route path="destacados" element={<Destacados />} />
+            <Route path="catalogo" element={<Catalogo />} />
+            <Route path="login" element={<Login />} />
+            <Route path="aboutus" element={<AboutUs />} />
           </Route>
         </Routes>
       </Router>
