8d5237c07a7175d991c0b6f6d1ca2152f98c131a
EmmanuelBaena69 <travispastrana075@gmail.com>
2023-11-16 18:06:25 -0500

    error en catalogo
    Error solucionado, causado por cambios en las ramas

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -29,5 +29,82 @@ const Catalogo = () => {
 
     console.log(seleccionados);
   };
+  return (
+    <>
+      <Slider />
+      <section className="mostrario">
+        <section className={`Filtro${openList ? "mostrar" : ""}`}>
+          <article className="Filtro__titulo">
+            <label id="titulo">FILTROS</label>
+            <hr />
+            <article className="Titulo_categorias">
+              <label htmlFor="Categorias" className="labels">
+                {" "}
+                Categorias{" "}
+              </label>
+              <input
+                type="checkbox"
+                id="Categorias"
+                className="input-checkbox"
+                onChange={cambio}
+              />
+            </article>
+          </article>
+
+          <ul className={`Filtro__Categorias${openList ? "mostrar" : ""}`}>
+            <li className="Categorias__li">
+              <label htmlFor="Romance"> Romance </label>
+              <input
+                name="Romance"
+                type="checkbox"
+                value={"Romance"}
+                id="Romance"
+                className="input-checkbox"
+                onChange={(e) => handleChecked(e)}
+              />
+            </li>
+
+            <li className="Categorias__li">
+              <label htmlFor="Comedia">Comedia</label>
+              <input
+                name="Comedia"
+                type="checkbox"
+                id="Comedia"
+                value={"Comedia"}
+                className="input-checkbox"
+                onChange={(e) => handleChecked(e)}
+              />
+            </li>
+
+            <li className="Categorias__li">
+              <label htmlFor="Accion"> Accion </label>
+              <input
+                name="Accion"
+                type="checkbox"
+                id="Accion"
+                value={"Accion"}
+                className="input-checkbox"
+                onChange={(e) => handleChecked(e)}
+              />
+            </li>
+          </ul>
+        </section>
+
+        <section className="CP">
+          {openList
+            ? animes
+              .filter(({ categoria }) =>
+                categoria.some((cat) => seleccionados.includes(cat))
+              )
+              .map((elemento) => (
+                <Card key={elemento.titulo} paquete={elemento} />
+              ))
+            : animes.map((elemento) => (
+              <Card key={elemento.titulo} paquete={elemento} />
+            ))}
+        </section>
+      </section>
+    </>
+  );
 };
 export default Catalogo;
